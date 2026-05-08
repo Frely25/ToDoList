5948bfeb1926ffd7e4b4d701d93711eaa0714c9d
Frely25 <okefrely@gmail.com>
2026-05-08 03:37:41 +0300

    Сделал скелет для файла с историями

--- internal/history/history.go
+++ internal/history/history.go
@@ -1,1 +1,13 @@
 package history
+
+type History struct {
+	path string
+}
+
+func NewHistory(path string) *History {
+	return &History{}
+}
+
+func (h History) Log(message string) {
+	// Логгируем в формате [время дата] действие объект
+}
