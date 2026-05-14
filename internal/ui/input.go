c885a3a4dc1cd2fab47ccb08e84ef530060df77c
Frely25 <okefrely@gmail.com>
2026-05-14 16:29:52 +0300

    Прописал readInt и readString

--- internal/ui/input.go
+++ internal/ui/input.go
@@ -1,11 +1,31 @@
 package ui
 
-func ReadInt() (int, error) {
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
 
-	return 0, nil
+var reader = bufio.NewReader(os.Stdin)
+
+func ReadInt() (int, error) {
+	input, err := reader.ReadString('\n') // Читаем строку до символа новой строки
+	if err != nil {                       // Если произошла ошибка при чтении, возвращаем её
+		return 0, err
+	}
+	input = strings.TrimSpace(input)                   // Удаляем пробелы и символы новой строки
+	if value, err := strconv.Atoi(input); err == nil { // Преобразуем строку в целое число
+		return value, nil // Если преобразование прошло успешно, возвращаем число и nil (нет ошибки)
+	}
+	return 0, errors.New("invalid input: not an integer") // Если преобразование не удалось, возвращаем 0 и ошибку
 }
 
 func ReadLine() string {
-
-	return ""
+	input, err := reader.ReadString('\n') // Читаем строку до символа новой строки
+	if err != nil {                       // Если произошла ошибка при чтении, возвращаем пустую строку
+		return ""
+	}
+	return strings.TrimSpace(input) // Удаляем пробелы и символы новой строки и возвращаем результат
 }
