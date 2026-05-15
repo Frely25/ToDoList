849f4461e6a5ff2670991fc915a10234719b4589
Frely25 <okefrely@gmail.com>
2026-05-15 14:09:04 +0300

    tt

--- internal/task/file_storage.go
+++ internal/task/file_storage.go
@@ -1,14 +1,31 @@
 package task
 
+import "os"
+
 type FileStorage struct {
 	path string
 }
 
+const path_to_data string = "../../data/"
+
 func NewFileStorage(path string) *FileStorage {
 	// Проверить существует ли такой файл
-	// Если нет, создать, если да - открыть
-	// Вернуть структуру с заполненым путем
-	return &FileStorage{}
+	if path == "" {
+		path = path_to_data + "tasks.json"
+	}
+	file, err := os.Open(path_to_data + path)
+	if err != nil {
+		// Если нет, создать, если да - открыть
+		file, err = os.Create(path_to_data + path)
+		if err != nil {
+			return nil
+		}
+
+	}
+	defer file.Close()
+	return &FileStorage{ // Вернуть структуру с заполненым путем
+		path: path_to_data + path,
+	}
 }
 
 func (file FileStorage) Save(task_to_save []Task) error {
@@ -25,5 +42,9 @@ func (file FileStorage) Load() ([]Task, int, error) {
 }
 
 func (file FileStorage) LoadNextId() (int, error) {
+	// file, err := os.OpenFile(path_to_data+"config.txt", os.O_RDWR, 0644)
+	// if err != nil {
+	// 	return 0, err
+	// }
 	return 0, nil // Возвращаем счетсчик для id
 }
