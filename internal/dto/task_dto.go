7a222175ab95fa5007f7cc3e440f0db2427f8d66
Frely25 <okefrely@gmail.com>
2026-06-22 20:43:08 +0300

    Прописали Routes и добавили хендлеры

--- internal/dto/task_dto.go
+++ internal/dto/task_dto.go
@@ -1,1 +1,23 @@
 package dto
+
+import "time"
+
+type CreateTaskRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"descriprion"`
+}
+
+type UpdateTaskRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"descriprion"`
+	Completed   bool   `json:"completed"`
+}
+
+type ResponseTask struct {
+	ID            int       `json:"id"`
+	Title         string    `json:"title"`
+	Description   string    `json:"description"`
+	DateCreate    time.Time `json:"datacreate"`
+	DateCompleted time.Time `json:"datacomp"`
+	Completed     bool      `json:"completed"`
+}
--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,1 +1,25 @@
 package server
+
+import (
+	"net/http"
+)
+
+func (s *Server) handleCreateTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) handleGetTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) handleGetTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) handleDeleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) handleUpdateTask(w http.ResponseWriter, r *http.Request) {
+
+}
